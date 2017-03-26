9baaba50961548e38f4237aeaa2ec77278642168
Codpoe <codpoe.me@gmail.com>
2017-03-27 02:17:21 +0800

    add s-radio

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
 import Vue from 'vue'
 import SButton from "../package/button/button.vue";
 import SButtonGroup from "../package/button-group/button-group.vue";
+import SRadio from "../package/radio/radio.vue";
 
 Vue.component(SButton.name, SButton);
 Vue.component(SButtonGroup.name, SButtonGroup);
+Vue.component(SRadio.name, SRadio);
 
-new Vue({
+let app = new Vue({
     el: '#app',
+    data: {
+        picked: "备选项 1"
+    },
     methods: {
         handleClick: function (ev) {
             console.log(ev);
+            console.log(this.picked);
         }
     }
 });
+
+
 
