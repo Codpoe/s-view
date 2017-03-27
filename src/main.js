165ee955e01001dbf9937bd99588a9aac5862bc2
Codpoe <codpoe.me@gmail.com>
2017-03-28 02:02:48 +0800

    add s-checkbox

--- src/main.js
+++ src/main.js
@@ -2,20 +2,24 @@ import Vue from 'vue'
 import SButton from "../package/button/button.vue";
 import SButtonGroup from "../package/button-group/button-group.vue";
 import SRadio from "../package/radio/radio.vue";
+import SCheckbox from "../package/checkbox/checkbox.vue";
 
 Vue.component(SButton.name, SButton);
 Vue.component(SButtonGroup.name, SButtonGroup);
 Vue.component(SRadio.name, SRadio);
+Vue.component(SCheckbox.name, SCheckbox);
 
 let app = new Vue({
     el: '#app',
     data: {
-        picked: "备选项 1"
+        picked: "备选项 1",
+        checked: ["备选项 3", "备选项 4"]
     },
     methods: {
         handleClick: function (ev) {
             console.log(ev);
             console.log(this.picked);
+            console.log(this.checked);
         }
     }
 });
