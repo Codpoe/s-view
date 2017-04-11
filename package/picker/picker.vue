250bfa315b8f21031bc428d5e8f1f755923948fc
Codpoe <codpoe.me@gmail.com>
2017-04-11 22:47:34 +0800

    update: s-picker supports actions(plus and minus)

--- package/picker/picker.vue
+++ package/picker/picker.vue
@@ -1,20 +1,32 @@
 <template>
     <div class="s-picker">
+        <div v-show="hasAction"
+             class="s-picker__action"
+             @click="onMinusClick">
+            <s-icon name="minus"></s-icon>
+        </div>
         <div class="s-picker__content"
-             @mouseleave="onMouseLeave"
-             ref="picker">
+             @mouseleave="onMouseLeave">
             <div class="s-picker__content__top"></div>
-            <ul>
-                <li></li>
-                <li v-for="item in items"
-                    :class="[
+            <div class="s-picker__content__list"
+                 ref="picker">
+                <ul>
+                    <li></li>
+                    <li v-for="item in items"
+                        :class="[
                         item === model ? 's-picker__item--active' : 's-picker__item--inactive'
                     ]">
-                    {{ item }}
+                        {{ item }}
                 </li>
-                <li></li>
-            </ul>
+                    <li></li>
+                </ul>
+            </div>
             <div class="s-picker__content__bottom"></div>
+        </div>
+        <div v-show="hasAction"
+             class="s-picker__action"
+             @click="onPlusClick">
+            <s-icon name="plus"></s-icon>
         </div>
     </div>
 </template>
@@ -23,14 +35,24 @@
     @import "../common/common.css";
 
     .s-picker {
-        display: inline-block;
-        position: relative;
-        .s-picker__content {
-            height: 79px;
-            overflow: scroll;
-            &::-webkit-scrollbar {
-                display: none;
+        display: inline-flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        .s-picker__action {
+            display: inline-flex;
+            i {
+                font-size: 12px;
+                color: var(--extra-light-silver);
+                cursor: pointer;
+                &:hover {
+                    color: var(--primary-color);
+                }
             }
+        }
+        .s-picker__content {
+            position: relative;
+            height: 79px;
             .s-picker__content__top {
                 position: absolute;
                 top: 26px;
@@ -50,24 +72,31 @@
                 /*background-color: var(--extra-light-gray);*/
                 opacity: 0.6;
             }
-            ul {
-                transition: all 0.25s;
-                li {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 25px;
-                    padding: 0 14px;
-                    margin: 1px 0;
-                    font-size: 14px;
+            .s-picker__content__list {
+                height: 100%;
+                overflow: scroll;
+                &::-webkit-scrollbar {
+                    display: none;
                 }
-                .s-picker__item--active {
-                    animation: s-picker__item--active-anim 0.25s;
-                    animation-fill-mode: both;
-                }
-                .s-picker__item--inactive {
-                    animation: s-picker__item--inactive-anim 0.25s;
-                    animation-fill-mode: both;
+                ul {
+                    transition: all 0.25s;
+                    li {
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 25px;
+                        padding: 0 14px;
+                        margin: 1px 0;
+                        font-size: 14px;
+                    }
+                    .s-picker__item--active {
+                        animation: s-picker__item--active-anim 0.25s;
+                        animation-fill-mode: both;
+                    }
+                    .s-picker__item--inactive {
+                        animation: s-picker__item--inactive-anim 0.25s;
+                        animation-fill-mode: both;
+                    }
                 }
             }
         }
@@ -97,8 +126,14 @@
 </style>
 
 <script>
-    module.exports = {
+    import SIcon from "../icon/icon.vue";
+
+    export default {
         name: "s-picker",
+
+        components: {
+            SIcon
+        },
 
         props: {
             items: Array,
@@ -106,6 +141,10 @@
             type: {
                 type: String,
                 default: "normal"
+            },
+            hasAction: {
+                type: Boolean,
+                default: false
             },
             disabled: {
                 type: Boolean,
@@ -129,6 +168,9 @@
                     console.log("s-picker set model: " + value);
                     this.$emit("input", value);
                 }
+            },
+            position: function () {
+                return this.items.indexOf(this.model);
             }
         },
 
@@ -145,6 +187,20 @@
         methods: {
             onMouseLeave: function (ev) {
                 this.$refs.picker.scrollTop = this.items.indexOf(this.model) * 26;
+            },
+
+            onMinusClick: function (ev) {
+                if (this.position === 0) {
+                    return;
+                }
+                this.model = this.items[this.position - 1];
+            },
+
+            onPlusClick: function (ev) {
+                if (this.position === this.items.length - 1) {
+                    return;
+                }
+                this.model = this.items[this.position + 1];
             },
 
             onScroll: function (ev) {
